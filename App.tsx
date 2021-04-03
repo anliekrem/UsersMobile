a33600853d9094d4e99fa9d38d27a39f39668795
ekremanli <ekrem.anli@perwatch.com>
2021-04-03 16:41:02 +0300

    eslint config updates

--- App.tsx
+++ App.tsx
@@ -10,59 +10,59 @@
 
 import React from 'react'
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
 } from 'react-native'
 
 import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions
 } from 'react-native/Libraries/NewAppScreen'
 
 const Section: React.FC<{
     title: string
-}> = ({children, title}) => {
-    const isDarkMode = useColorScheme() === 'dark'
-    return (
+}> = ({ children, title }) => {
+  const isDarkMode = useColorScheme() === 'dark'
+  return (
         <View style={styles.sectionContainer}>
             <Text
                 style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black
-                    }
+                  styles.sectionTitle,
+                  {
+                    color: isDarkMode ? Colors.white : Colors.black
+                  }
                 ]}>
                 {title}
             </Text>
             <Text
                 style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark
-                    }
+                  styles.sectionDescription,
+                  {
+                    color: isDarkMode ? Colors.light : Colors.dark
+                  }
                 ]}>
                 {children}
             </Text>
         </View>
-    )
+  )
 }
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark'
+  const isDarkMode = useColorScheme() === 'dark'
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
-    }
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  }
 
-    return (
+  return (
         <SafeAreaView style={backgroundStyle}>
             <StatusBar
                 barStyle={isDarkMode ? 'light-content' : 'dark-content'}
@@ -73,9 +73,9 @@ const App = () => {
                 <Header />
                 <View
                     style={{
-                        backgroundColor: isDarkMode
-                            ? Colors.black
-                            : Colors.white
+                      backgroundColor: isDarkMode
+                        ? Colors.black
+                        : Colors.white
                     }}>
                     <Section title="Step One">
                         Edit <Text style={styles.highlight}>App.js</Text> to
@@ -94,26 +94,26 @@ const App = () => {
                 </View>
             </ScrollView>
         </SafeAreaView>
-    )
+  )
 }
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600'
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400'
-    },
-    highlight: {
-        fontWeight: '700'
-    }
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400'
+  },
+  highlight: {
+    fontWeight: '700'
+  }
 })
 
 export default App
